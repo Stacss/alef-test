b1e922fa0307f8b88a3cd6617293b3668c1511df
Stas <postnikov.sa@ya.ru>
2023-11-15 00:04:31 +0700

    update Lecture model

--- app/Lecture.php
+++ app/Lecture.php
@@ -6,15 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lecture extends Model
 {
-    protected $fillable = ['topic', 'description'];
+    protected $fillable = [
+        'name',
+        'description'
+    ];
 
-    public function courses()
+    public function attendedBy()
     {
-        return $this->belongsToMany(Course::class, 'course_lecture', 'lecture_id', 'course_id');
-    }
-
-    public function students()
-    {
-        return $this->belongsToMany(Student::class, 'student_lecture', 'lecture_id', 'student_id');
+        return $this->belongsToMany(Student::class, 'attended_lectures', 'lecture_id', 'student_id')->withTimestamps();
     }
 }
