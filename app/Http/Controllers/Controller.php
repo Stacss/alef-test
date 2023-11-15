6a44534838e75d2c016f860f3e7c9c2b42bd25b1
Stas <postnikov.sa@ya.ru>
2023-11-15 11:58:26 +0700

    instruction swagger in Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,16 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Alef-test Api",
+ *      description="L5 Swagger OpenApi description Alef-test",
+ *      @OA\Contact(
+ *          email="postnikov.sa@ya.ru"
+ *      )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
