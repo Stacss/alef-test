31315b06eb2df160fba9277b102b624eb3936c42
Stas <postnikov.sa@ya.ru>
2023-11-16 23:49:20 +0700

    update deleteLecture, updateLecture methods

--- app/Http/Controllers/LectionController.php
+++ app/Http/Controllers/LectionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Lecture;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
@@ -93,6 +94,132 @@ class LectionController extends Controller
             return response()->json(['message' => 'Validation error', 'errors' => $e->errors()], 422);
         } catch (\Exception $e) {
             return response()->json(['message' => 'Error creating lection', 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * @OA\Put(
+     *      path="/api/lections/{id}",
+     *      operationId="updateLecture",
+     *      tags={"Lections"},
+     *      summary="Update a lecture",
+     *      description="Update an existing lecture's details.",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Lecture ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="Updated lecture data",
+     *          @OA\JsonContent(
+     *              required={"name", "description"},
+     *              @OA\Property(property="name", type="string", example="Updated Lecture Name"),
+     *              @OA\Property(property="description", type="string", example="Updated Lecture Description")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Lecture updated successfully",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Lecture updated successfully"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Validation error",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Validation error"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Error updating lecture",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Error updating lecture"),
+     *              @OA\Property(property="error", type="string", example="Internal Server Error")
+     *          )
+     *      )
+     * )
+     */
+    public function updateLecture(Request $request, $id)
+    {
+        try {
+            $lecture = Lecture::findOrFail($id);
+
+            $this->validate($request, [
+                'name' => 'required|string|max:255|unique:lectures,name,' . $id,
+                'description' => 'required|string',
+            ]);
+
+            $data = $request->only(['name', 'description']);
+            $lecture->update($data);
+
+            return response()->json(['message' => 'Lecture updated successfully', 'data' => $lecture], 200);
+        } catch (ValidationException $e) {
+            return response()->json(['message' => 'Validation error', 'errors' => $e->errors()], 422);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['message' => 'Lecture not found'], 404);
+        }catch (\Exception $e) {
+            return response()->json(['message' => 'Error updating lecture', 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * @OA\Delete(
+     *      path="/api/lections/{id}",
+     *      operationId="deleteLecture",
+     *      tags={"Lections"},
+     *      summary="Delete a lecture",
+     *      description="Delete an existing lecture.",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Lecture ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Lecture deleted successfully",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Lecture deleted successfully")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Lecture not found",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Lecture not found")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Error deleting lecture",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Error deleting lecture"),
+     *              @OA\Property(property="error", type="string", example="Internal Server Error")
+     *          )
+     *      )
+     * )
+     */
+    public function deleteLecture($id)
+    {
+        try {
+            $lecture = Lecture::findOrFail($id);
+            $lecture->delete();
+
+            return response()->json(['message' => 'Lecture deleted successfully'], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['message' => 'Lecture not found'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error deleting lecture', 'error' => $e->getMessage()], 500);
         }
     }
 
